9b65dbfea282380f167ae4ed38ea100877cac685
Prosper Ndebele <pndebele@wesleyan.edu>
2023-10-17 20:53:53 -0400

    add a function

--- print_hello.py
+++ print_hello.py
@@ -1,2 +1,11 @@
 print("hello Prosper, How are you?")
 print("what happend?")
+# reverse a list in place
+
+
+def reverse(arr):
+    l, r = 0, len(arr)-1
+    while l < r:
+        arr[l], arr[r] = arr[r], [l]
+        l, r = l + 1, r - 1
+    return arr
